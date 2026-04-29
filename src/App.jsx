df5a94d206175fc5e40e626e5bf994dcc2fc1b14
Андрій <andriystetsula2344gmail.com@MacBook-Pro-Andrij.local>
2026-04-29 18:14:34 +0300

    git add solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -14,33 +15,72 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button type="button" className="button is-info is-light">
-        Sort alphabetically
-      </button>
+export const App = () => {
+  const [goodState, setGoodState] = useState(goodsFromServer);
+  const [goodStyle, setGoodStyle] = useState(null);
+  const [isReverse, setIsReverse] = useState(false);
 
-      <button type="button" className="button is-success is-light">
-        Sort by length
-      </button>
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          className={`button is-info ${goodStyle === 'alpha' ? 'is-light' : ''}`}
+          onClick={() => {
+            setGoodState([...goodState].sort((a, b) => a.localeCompare(b)));
+            setGoodStyle('alpha');
+            setIsReverse(false);
+          }}
+        >
+          Sort alphabetically
+        </button>
 
-      <button type="button" className="button is-warning is-light">
-        Reverse
-      </button>
+        <button
+          type="button"
+          className={`button is-success ${goodStyle === 'length' ? 'is-light' : ''}`}
+          onClick={() => {
+            setGoodState([...goodState].sort((a, b) => a.length - b.length));
+            setGoodStyle('length');
+            setIsReverse(false);
+          }}
+        >
+          Sort by length
+        </button>
 
-      <button type="button" className="button is-danger is-light">
-        Reset
-      </button>
+        <button
+          type="button"
+          className={`button is-warning ${goodStyle ? 'is-light' : ''}`}
+          onClick={() => {
+            setGoodState([...goodState].reverse());
+            setGoodStyle(false);
+            setIsReverse(!isReverse);
+          }}
+        >
+          Reverse
+        </button>
+
+        {goodState !== goodsFromServer && (
+          <button
+            type="button"
+            className={`button is-danger ${goodStyle === 'reset' ? 'is-light' : ''}`}
+            onClick={() => {
+              setGoodState(goodsFromServer);
+              setGoodStyle('reset');
+              setIsReverse(false);
+            }}
+          >
+            Reset
+          </button>
+        )}
+      </div>
+
+      <ul>
+        {goodState.map(item => (
+          <li key={item} data-cy="Good">
+            {item}
+          </li>
+        ))}
+      </ul>
     </div>
-
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+  );
+};
